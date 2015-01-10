141f3c3c7037a6a1208d5c283c547159198e29a5
EricSchles <ericschles@gmail.com>
2015-01-10 18:05:41 -0500

    building a doubly linked list

--- LinkedList.java
+++ LinkedList.java
@@ -1,17 +1,20 @@
 class Node<T>{
 	T data;
 	Node next;
-	<T> Node(T data){
+	Node prev;
+	Node(T data){
 		this.data = data;
 		this.next = null;
+		this.prev = null;
 	}
 	Node(){
 		this.data = null;
 		this.next = null;
+		this.prev = null;
 	}
 }
 
-public LinkedList<T>{
+public class LinkedList<T>{
 	Node head;
 	Node tail;
 
@@ -19,4 +22,30 @@ public LinkedList<T>{
 		this.head = new Node(data);
 		this.tail = this.head;
 	}
+	public <T> LinkedList(){
+		this.head = new Node();
+		this.tail = this.head;
+	}
+
+	public <T> void add(T data){
+		if(this.head == null){
+			this.head = new Node(data);
+			this.tail = this.head;
+		}else if(){
+			Node new_node = new Node(data);
+			new_node = this.tail.next;
+			this.tail = new_node;
+		}
+	}
+
+	public String toString(){
+		Node cur = this.head;
+		String str = "";
+		while(cur.next != null){
+			str += cur.data;
+			cur = cur.next;
+		}
+		return str;
+	}
+
 }
---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main{
+	public static <T> void print(T input){
+		System.out.println(input);
+	}
+	public static void main(String[] args){
+		LinkedList ll = new LinkedList();
+		ll.add(5);
+		ll.add(7);
+		print(ll.toString());
+	}
+}
